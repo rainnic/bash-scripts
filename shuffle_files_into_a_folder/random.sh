abf63cbf96d576fbc4e90788a94b9675ae4c1204
rainnic <rainnic@altervista.org>
2017-02-09 19:17:52 +0100

    added clean/shuffle option and help

--- shuffle_files_into_a_folder/random.sh
+++ shuffle_files_into_a_folder/random.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #
+# MP3 with target Volume sets to 95 db
+# Last edited on 2017/01/14 --> added a clean/shuffle option and help
+#
 # SETTING:
 EXTENSION_IN="mp3" # initial extension
 EXTENSION_OUT="mp3" # final extension
 RM_FILE=false  # if true, delete initial file after executing command
 DEBUG=false    # if true, print statement without executing any command
+CLEAN=false    # if true, only clean all the file
 OPTION="0"
 #
 # Description of the options:
@@ -17,16 +21,33 @@ OPTION="0"
 #
 
 # Check if the user specifies a destination directory
-if [ -n "$1" ]
+
+if [ -z "$1" ] || [ -z "$2" ]
   then
-   directory=$1 # use this new working directory
+   echo "Usage: -clean or -shuffle and directory"
+   echo ""
+   exit 0
+fi
+
+if [ "$1" == "-clean" ]
+  then
+   CLEAN=true # if true, only clean all the file
+fi
+
+if [ -n "$2" ]
+  then
+   directory=$2 # use this new working directory
   else
    directory=$PWD # use the same directory where you have launched the script
 fi
 
+
 eval cd $directory
 OLDIFS=$IFS
 IFS=$'\n'
+container=()
+fiter=0
+iterazioni=28
 
 for file in `ls *.${EXTENSION_IN}`
 do
@@ -34,11 +55,61 @@ do
    newfilename=`echo $newfilename | sed 's/(//g;s/)//g'` # remove "(" and ")"
    newfilename=`echo $newfilename | sed 's/-//g'` # remove "-"
    newfilename=`echo $newfilename | sed "s/'/_/g"` # remove "'" and put "_"
-   newfilename=`echo $newfilename | sed 's/^[a-z]_//g'` # remove first "alpha" + "_"
+   newfilename=`echo $newfilename | sed 's/^[a-z0-9]_//g'` # remove first "alpha" + "_"
 
-   # Add random character at the beginning
-   shuffle=`cat /dev/urandom | tr -cd 'a-z' | head -c 1`
-   newfilename="$shuffle"_"$newfilename"
+   # Vecchio random Add random character at the beginning
+   #shuffle=`cat /dev/urandom | tr -cd 'a-z0-9' | head -c 1`
+   #newfilename="$shuffle"_"$newfilename"
+
+if [ "$CLEAN" == false ]; then
+# INIZIO NUOVO
+
+# Add random character at the beginning
+   shuffle=`cat /dev/urandom | tr -cd 'a-z0-9' | head -c 1`
+   fiter=$[$fiter+1]
+   echo "Il file processato è il numero $fiter"
+
+if [ "${#container[*]}" == 0 ]
+   then
+      container=($shuffle)
+      newfilename="$shuffle"_"$newfilename"
+      echo "la prima matrice è ${container[@]}"
+      echo "La matrice ora contiene ${#container[*]} elementi"
+   else
+      iter=${#container[*]}
+      echo "La matrice contiene $iter elementi"
+      if [ $iter -lt $iterazioni ] 
+         then
+         shuffle=`cat /dev/urandom | tr -cd 'a-z0-9' | head -c 1`
+                          # fonte: http://stackoverflow.com/questions/3685970/check-if-an-array-contains-a-value
+                          if [[ " ${container[@]} " =~ " ${shuffle} " ]]; then
+                            while [[ " ${container[@]} " =~ " ${shuffle} " ]]
+                               do
+                               shuffle=`cat /dev/urandom | tr -cd 'a-z0-9' | head -c 1`
+                            done
+                            container[$iter]=$shuffle
+                            newfilename="$shuffle"_"$newfilename"
+                            echo "UGUALE La matrice ora contiene ${#container[*]} elementi"
+                          else
+                            container[$iter]=$shuffle
+                            newfilename="$shuffle"_"$newfilename"
+                            echo "DIFFERENTE La matrice ora contiene ${#container[*]} elementi"
+                          fi
+         echo "la nuova matrice è ${container[@]}"
+         echo " "
+         else
+         container=()
+         shuffle=`cat /dev/urandom | tr -cd 'a-z0-9' | head -c 1`
+      container=($shuffle)
+      newfilename="$shuffle"_"$newfilename"
+      echo "la prima matrice è ${container[@]}"
+      echo "La matrice ora contiene ${#container[*]} elementi"
+         echo "la nuova matrice è ${container[@]}"
+      fi
+fi
+
+# FINE NUOVO
+fi
 
    # Remove the extension
    newfile=${newfilename%.*}
